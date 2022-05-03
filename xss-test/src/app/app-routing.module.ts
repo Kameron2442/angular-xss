a09a716fcf747f3734366cadb8eb1209bbb614e6
Kameron-2332 <41551432+Kameron-2332@users.noreply.github.com>
2022-05-03 18:51:13 -0400

    Removed default html and added added my xss component to test with

--- xss-test/src/app/app-routing.module.ts
+++ xss-test/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { XssComponent } from './components/xss/xss.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: '', component: XssComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- xss-test/src/app/app.module.ts
+++ xss-test/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { XssComponent } from './components/xss/xss.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    XssComponent
   ],
   imports: [
     BrowserModule,
